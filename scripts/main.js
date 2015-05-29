26892e094350753c012b3c90d7b47e6f49533b25
Nan Chen <nchen333@gmail.com>
2015-05-28 20:56:32 -0400

    authorization setup

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,35 @@
+// (function(){
+//   'use strict';
+//
+//   $(document).ready(function(){
+//     $('body').prepend(JST['application']());
+//   });
+// })();
+
 (function(){
   'use strict';
 
   $(document).ready(function(){
-    $('body').prepend(JST['application']());
+    $.ajax({
+      url: "https://api.github.com/user",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(user) {
+      console.log(user);
+    });
+  });
+
+  $(document).ready(function(e){
+    var code = window.location.href.match(/\?code=(.*)/)[1];
+    if(code) {
+      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
+       console.log(data);
+      });
+    }
+  });
+
+  $('button').on('click', function(e){
+    window.location.replace('https://github.com/login/oauth/authorize?client_id=365d670e510fb8a44ecb');
   });
 })();
