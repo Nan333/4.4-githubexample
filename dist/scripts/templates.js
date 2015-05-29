5bd7999764805fe810749e396d41f47cc664aced
Nan Chen <nchen333@gmail.com>
2015-05-29 15:08:53 -0400

    Start user template

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -2,3 +2,10 @@ this["JST"] = this["JST"] || {};
 this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<h1>Hello</h1>\n";
 },"useData":true});
+this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper;
+
+  return "<h1>"
+    + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>\n";
+},"useData":true});
