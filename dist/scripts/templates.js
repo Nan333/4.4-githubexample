c3b1799127a535889927e23352ae549e04bcc167
Nan Chen <nchen333@gmail.com>
2015-05-30 00:16:12 -0400

    boilerplate

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -1,11 +1,22 @@
 this["JST"] = this["JST"] || {};
-this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<h1>Hello</h1>\n";
-},"useData":true});
-this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+this["JST"]["repositories"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "<h1>"
+  return "  <li>"
     + this.escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h1>\n";
+    + "</li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "<h1>Repositories</h1>\n"
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "<h1>"
+    + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</h1>\n<h2>"
+    + alias3(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"login","hash":{},"data":data}) : helper)))
+    + "</h2>\n";
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,8 @@
-// (function(){
-//   'use strict';
-//
-//   $(document).ready(function(){
-//     $('body').prepend(JST['application']());
-//   });
-// })();
-
 (function(){
   'use strict';
 
   $(document).ready(function(){
+
     $.ajax({
       url: "https://api.github.com/user",
       headers: {
@@ -17,19 +10,18 @@
       }
     }).then(function(user) {
       console.log(user);
+      console.log(JST['user'](user));
+      $(".sidebar").append(JST['user'](user));
     });
-  });
 
-  $(document).ready(function(e){
-    var code = window.location.href.match(/\?code=(.*)/)[1];
-    if(code) {
-      $.getJSON('http://localhost:9999/authenticate/'+code, function(data) {
-       console.log(data);
-      });
-    }
-  });
-
-  $('button').on('click', function(e){
-    window.location.replace('https://github.com/login/oauth/authorize?client_id=365d670e510fb8a44ecb');
+    $.ajax({
+      url: "https://api.github.com/user/repos",
+      headers: {
+        "Authorization": "token " + GITHUB_TOKEN
+      }
+    }).then(function(repos) {
+      console.log(repos);
+      $(".repositories").append(JST['repositories'](repos));
+    });
   });
 })();
