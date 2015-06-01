ce3482ee4a92493dbf3ad1eeb40d80b3f44bf7d1
Nan Chen <nchen333@gmail.com>
2015-05-31 22:21:47 -0400

    sidebar pic added

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -8,15 +8,18 @@ this["JST"]["repositories"] = Handlebars.template({"1":function(depth0,helpers,p
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<h1>Repositories</h1>\n"
-    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
+  return "<section class='content'>\n  <h1>Repositories</h1>\n"
+    + ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "</section>\n";
 },"useData":true});
 this["JST"]["user"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<h1>"
+  return "<sidebar><img class=\"sidebar\" src='"
+    + alias3(((helper = (helper = helpers.avatar_url || (depth0 != null ? depth0.avatar_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"avatar_url","hash":{},"data":data}) : helper)))
+    + "'></img>\n<h1>"
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
     + "</h1>\n<h2>"
     + alias3(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"login","hash":{},"data":data}) : helper)))
-    + "</h2>\n";
+    + "</h2>\n\n</sidebar>\n";
 },"useData":true});
